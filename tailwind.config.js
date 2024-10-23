f7b3ea82b1907ca45d56e955534b428678da9b16
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2024-10-23 00:22:27 -0300

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Archivos donde Tailwind aplicará los estilos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
